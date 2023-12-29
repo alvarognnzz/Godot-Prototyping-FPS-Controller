73e3e1525fc9607dc4ad3068d01bf0e03a033fde
Álvaro González <alvgonz.ojeda@gmail.com>
2023-12-29 13:27:27 +0100

    [dev] basic state machine

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody3D
 
+enum State {IDLE, WALKING, JUMPING, SPRINTING, CROUCHING}
+
 @export_group("Nodes")
 @export var head: Node3D
 @export var camera: Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const MOUSE_SENSIBILITY = 0.1
+@export_range(1, 10, 0.1) var WALKING_SPEED: float = 4.5
+@export_range(1, 10, 0.1) var SPRINTING_SPEED: float = 6.0
+@export_range(1, 10, 0.1) var CROUCHING_SPEED: float = 3.5
+@export_range(1, 10, 0.1) var JUMP_VELOCITY: float = 4
+@export_range(0, 1, 0.1) var MOUSE_SENSIBILITY: float = 0.1
+
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var current_state = State.IDLE
+var speed = WALKING_SPEED
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -20,19 +27,53 @@ func _ready() -> void:
 	#crouch_shapecast.add_exception($".")
 
 func _physics_process(delta: float) -> void:
+	$"../Label".text = State.keys()[current_state]
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	state_machine()
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	move(speed)
 
-	var input_dir := Input.get_vector("left", "right", "foward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	match current_state:
+		State.IDLE:
+			pass
+			
+		State.WALKING:
+			speed = WALKING_SPEED
+		
+		State.JUMPING:
+			pass
+		
+		State.SPRINTING:
+			speed = SPRINTING_SPEED
+		
+		State.CROUCHING:
+			speed = CROUCHING_SPEED
 
 	move_and_slide()
+
+func state_machine() -> void:
+	if Input.get_vector("left", "right", "foward", "backward") == Vector2.ZERO and is_on_floor():
+		current_state = State.IDLE
+	if Input.get_vector("left", "right", "foward", "backward") != Vector2.ZERO and is_on_floor():
+		current_state = State.WALKING
+	if not is_on_floor():
+		current_state = State.JUMPING
+	if Input.is_action_pressed("sprint") and is_on_floor():
+		current_state = State.SPRINTING
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		current_state = State.CROUCHING
+
+func move(speed: float) -> void:
+	var input_dir: Vector2 = Input.get_vector("left", "right", "foward", "backward")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
